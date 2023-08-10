d6c420c133a5324f2c5ad1f5b434dcd131305e30
BezdninAlex <141863214+BezdninAlex@users.noreply.github.com>
2023-08-10 20:48:17 +0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -1,8 +1,9 @@
 document.querySelector(".generation").onclick = myClick;
 
 function myClick() {
-  let description = document.querySelector(".description").value;
   let brand = document.querySelector(".name").value;
+  let poduct = document.querySelector(".product").value;
   let sostav = document.querySelector(".sostavA").value;
   let category = document.querySelector(".category").value;
+  let description = document.querySelector(".description").value;
 }

fd4776047d1fd105fb25e200da719b1a44f69b26
BezdninAlex <141863214+BezdninAlex@users.noreply.github.com>
2023-08-11 15:24:41 +0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -11,3 +11,10 @@ async function Click_gen_feedback() {
   let description = document.querySelector(".description").value;
   document.querySelector(".feedback").value = await eel.generate_feedbackJS(discription)();
   }
+function getPrediction() {
+  let cost = document.querySelector(".cost").value;
+  let discount = document.querySelector(".discount").value;
+  let rating = document.querySelector(".rating").value;
+  let inStock = document.querySelector(".inStock").value;
+  let Feedback = document.querySelector(".Feedback").value;
+}

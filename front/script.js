7c636d2f5d6bfa47e3b3368b40eb2a69c00c18cb
CHex0K <116585490+CHex0K@users.noreply.github.com>
2023-08-11 11:41:25 +0300

    Update script.js

--- front/script.js
+++ front/script.js
@@ -1,10 +1,13 @@
-document.querySelector(".generation").onclick = myClick;
-
-function myClick() {
+async function myClick() {
   let brand = document.querySelector(".name").value;
-  let poduct = document.querySelector(".product").value;
+  let product = document.querySelector(".product").value;
   let sostav = document.querySelector(".sostavA").value;
   let category = document.querySelector(".category").value;
+  document.querySelector(".description").value = await eel.generate_descriptionJS(category,product,brand,sostav)();
+  }
+
+
+async function Click_gen_feedback() {
   let description = document.querySelector(".description").value;
-}
-function Click_gen_feedback() {}
+  document.querySelector(".feedback").value = await eel.generate_feedbackJS(discription)();
+  }
